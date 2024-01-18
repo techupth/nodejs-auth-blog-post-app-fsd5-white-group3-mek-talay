ee0199a4a1010d1849702d3b0e0a570276fe9be0
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-18 15:06:42 +0700

    feat: add completed exercise part 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,13 +4,16 @@ import App from "./App";
 import "./index.css";
 import { AuthProvider } from "./contexts/authentication";
 import { BrowserRouter } from "react-router-dom";
+import jwtInterceptor from "./utils/jwtInterceptor.js";
+
+jwtInterceptor();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+    <React.StrictMode>
+        <BrowserRouter>
+            <AuthProvider>
+                <App />
+            </AuthProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 );
--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -1,28 +1,41 @@
 import axios from "axios";
 
 function jwtInterceptor() {
-  axios.interceptors.request.use((req) => {
-    // 🐨 Todo: Exercise #6
-    //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
-    // เมื่อมีการส่ง Request จาก Client ไปหา Server
-    // ภายใน Callback Function axios.interceptors.request.use
+    axios.interceptors.request.use((req) => {
+        // 🐨 Todo: Exercise #6
+        //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
+        // เมื่อมีการส่ง Request จาก Client ไปหา Server
+        // ภายใน Callback Function axios.interceptors.request.use
+        const token = window.localStorage.getItem("token");
+        const hasToken = Boolean(token);
 
-    return req;
-  });
+        if (hasToken) {
+            req.headers = {
+                ...req.headers,
+                Authorization: `Bearer ${token}`,
+            };
+        }
 
-  axios.interceptors.response.use(
-    (req) => {
-      return req;
-    },
-    (error) => {
-      // 🐨 Todo: Exercise #6
-      //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
-      // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
-      // ภายใน Error Callback Function ของ axios.interceptors.response.use
+        return req;
+    });
 
-      return Promise.reject(error);
-    }
-  );
+    axios.interceptors.response.use(
+        (req) => {
+            return req;
+        },
+        (error) => {
+            // 🐨 Todo: Exercise #6
+            //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
+            // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
+            // ภายใน Error Callback Function ของ axios.interceptors.response.use
+            if (error.response.status === 401 && error.response.statusText === "Unauthorized") {
+                window.localStorage.removeItem("token");
+                window.localStorage.replace("/");
+            }
+
+            return Promise.reject(error);
+        }
+    );
 }
 
 export default jwtInterceptor;
