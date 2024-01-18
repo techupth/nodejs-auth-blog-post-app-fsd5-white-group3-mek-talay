bfe506c63f16ce6189e3387d5517fa5a91cb26f4
Pacharadanai <pacharadanaipinake@gmail.com>
2024-01-18 13:13:06 +0700

    feat: add completed exercise part 1

--- server/app.js
+++ server/app.js
@@ -2,29 +2,35 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import postRouter from "./apps/posts.js";
+import authRouter from "./apps/auth.js";
 import { client } from "./utils/db.js";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
 
 async function init() {
-  const app = express();
-  const port = 4000;
+    const app = express();
+    const port = 4000;
 
-  await client.connect();
+    dotenv.config();
 
-  app.use(cors());
-  app.use(bodyParser.json());
-  app.use("/posts", postRouter);
+    await client.connect();
 
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
-  });
+    app.use(cors());
+    app.use(bodyParser.json());
+    app.use("/posts", postRouter);
+    app.use("/auth", authRouter);
 
-  app.get("*", (req, res) => {
-    res.status(404).send("Not found");
-  });
+    app.get("/", (req, res) => {
+        res.send("Hello World!");
+    });
 
-  app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  });
+    app.get("*", (req, res) => {
+        res.status(404).send("Not found");
+    });
+
+    app.listen(port, () => {
+        console.log(`Example app listening on port ${port}`);
+    });
 }
 
 init();
